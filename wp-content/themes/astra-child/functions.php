4999817ce8d7f3e84e813e1de924e270afcc788a
Jakub <j.poskrobko@goapps.pl>
2022-02-10 08:00:59 +0100

    'Fix url at styles'

--- wp-content/themes/astra-child/functions.php
+++ wp-content/themes/astra-child/functions.php
@@ -4,11 +4,11 @@
 function my_custom_styles() {
   // Register my custom stylesheet
 
-  wp_register_style('astra-theme-css', 'http://zryjto.linuxpl.info/platforma_dev/wp-content/themes/astra/assets/css/minified/main.min.css', array(), time(), 'all');
+  wp_register_style('astra-theme-css', home_url().'/wp-content/themes/astra/assets/css/minified/main.min.css', array(), time(), 'all');
   // Load my custom stylesheet
   wp_enqueue_style('astra-theme-css');
 
-  wp_register_style('astra-theme-css-2', 'http://zryjto.linuxpl.info/platforma_dev/wp-content/themes/astra-child/style.css', array(), time(), 'all');
+  wp_register_style('astra-theme-css-2', home_url().'/wp-content/themes/astra-child/style.css', array(), time(), 'all');
   // Load my custom stylesheet
   wp_enqueue_style('astra-theme-css-2');
 
